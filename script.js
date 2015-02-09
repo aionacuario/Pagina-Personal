1cec6e0b0c7af2c7561fb3a702e2edd1aa8ef52e
christian203972 <carlos_bm50@hotmail.com>
2015-02-09 17:02:03 -0500

    segundo commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function abrir2paginas(){
+	window.open('menu.html', 'menu'); // aca se agrega  la ubicacion que quiero abrir y el segundo es el target, nombre que e asignado a esa parte
+	window.open('sobremi.html', 'contenido'); // aca se agrega  la ubicacion que quiero abrir y el segundo es el target, nombre que e asignado a esa parte
+}
+function abrir2paginasFamilia(){
+	window.open('paginas/menu_familiar.html', 'menu');
+	window.open('familia.html', 'contenido');
+}
+function abrir2paginasInteres(){
+	window.open('paginas/menu_interes.html', 'menu');
+	window.open('paginas/guitarra.html', 'contenido');
+}
+
